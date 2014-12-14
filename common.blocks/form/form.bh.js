707afff7868867b987f9dac85f8ae2d1a6c02215
Alexej Yaroshevich <alex@qfox.ru>
2014-12-14 21:58:28 +0400

    add bh templates

--- common.blocks/form/form.bh.js
+++ common.blocks/form/form.bh.js
@@ -1,1 +1,14 @@
-// TODO
+module.exports = function(bh) {
+
+    bh.match('form', function(ctx, json) {
+        ctx.tag('form');
+        ctx.js(true);
+
+        ctx.attrs({
+            action : json.action,
+            method : json.method
+        });
+
+    });
+
+};
